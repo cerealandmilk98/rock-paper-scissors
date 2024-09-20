4e5d5d5d54cc9ded462e8e9227d003b37256a07f
cerealandmilk98 <brody.allen.clark@gmail.com>
2024-09-20 13:31:53 -0500

    Finishing the get computers choice funtion.
    I created a funtion called getComputersChoice().
    The purpose of this function was to randomly
    choose between rock, paper, or scissors. We didn't
    have to add any arguments to the funtion because
    it didn't require any specific input. Now we had
    to add a lot to the body of the we added a
    constant variable named randomNumber and asigned
    it the Math object with a method of .random().
    After this we created a if statement. The if
    statement said if the randomNumber was less than
    0.3 return "rock", if else the randomNumber was less
    than 0.6 then return "paper", and else the
    randomNumber was higher than 0.6 return scissors.
    All of this was returned in the console with
    console.log(getComputersChoice());.

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 // Made a script.js connected to my index.html.
-console.log("Hello World")
+
+// We will try to write a funtion named getComputerChoice. We want this function to randomly return one of the following string values: "rock", "paper", or "scissors".
+// First we created a function and named it get ComputetChoice() to show it was the computers choice on the random number. We didn't put a argument in the () because it didn't have a specific input. In the function body we put constant variable named randomNumber and assigned it the the Math object with the .random method.
+// After the randome number variable we created a if statement conditional. if random number was less than 0.3 return rock, else if random number was less than 0.6 return paper, else return scissors if it's more than 0.6.
+function getComputerChoice() {
+  const randomNumber = Math.random(); // Get a random number between 0 and 1
+  if (randomNumber < 0.3) {
+    return "rock"; // If randomNumber is less than 0.3, return "rock"
+  } else if (randomNumber < 0.6) {
+    return "paper"; // If between 0.3 and 0.6, return "paper"
+  } else {
+    return "scissors"; // If greater than 0.6, return "scissors"
+  }
+}
+
+console.log(getComputerChoice());
